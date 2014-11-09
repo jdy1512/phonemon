2bfb8e5076b86ea757b81b7cc554f06d78eba6bb
jdy1512 <dyoung1512@gmail.com>
2014-11-09 16:18:41 +0900

    2014-11-09 16:18 jd

---
+++ PhoneMon/src/control/ModuleManager.java
@@ -0,0 +1,12 @@
+package control;
+
+public abstract interface ModuleManager {
+	
+	// kdh methods
+	String RSADoubleEncrypt(String privateKey, String publicKey);	// RSA 2중 암호 모듈, 서버공개키로 암호화 -> 본인 개인키로 암호화
+	
+	// jky methods
+	
+	
+	// jdy methods
+}
