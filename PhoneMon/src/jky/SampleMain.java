3112acc6065a4a9335cf034a113580f70594c1e2
jdy1512 <dyoung1512@gmail.com>
2014-11-09 16:27:49 +0900

    2014-11-09 16:27 jd

---
+++ PhoneMon/src/jky/SampleMain.java
@@ -0,0 +1,15 @@
+package jky;
+
+import control.ModuleManager;
+
+public class SampleMain implements ModuleManager{
+	public static void main(String[] args){
+		
+	}
+
+	@Override
+	public String RSADoubleEncrypt(String privateKey, String publicKey) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
